5bb7c3cc29ad52ee60e442a48cbdb522288e1898
Ebedi Meleck <ituzeebedi12@gmail.com>
2025-01-26 23:21:10 +0200

    modified some thing

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,10 +1,29 @@
 public class InsertionSort {
+    static void  InsertionSort(){
+        int[] arr = {12, 23, 23, 56 , 45,4};
+        for (int i = 1; i <arr.length ; i++) {
+            int temp = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j] >temp){
+               arr[j+1] = arr[j];
+               j--;
+            }
+            arr[j+1] =temp;
+        }
+
+        for(int a : arr){
+            System.out.printf("%d\t",a);
+        }
+
+    }
     public static void main(String[] args) {
         int[] array = {9,1,8,8,5,0,10};
         insertSort(array);
         for (int a :array){
             System.out.print(a+" ");
         }
+        System.out.println("\n");
+        InsertionSort();
     }
     static void insertSort(int[] array){
         for (int i = 1; i <array.length ; i++) {
